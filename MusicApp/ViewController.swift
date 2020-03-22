d513e6accd21098376be61cc672236b5d833d632
SangNX <nxsang063@gmail.com>
2020-03-22 10:44:51 +0700

    Initial Commit

---
+++ MusicApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by SangNX on 3/22/20.
+//  Copyright © 2020 SangNX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
